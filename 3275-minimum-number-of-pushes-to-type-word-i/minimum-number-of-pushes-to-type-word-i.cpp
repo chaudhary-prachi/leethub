5363a83e1ca29bdc8bc363559855502546c9f978
chaudhary-prachi <152068096+chaudhary-prachi@users.noreply.github.com>
2026-08-04 11:20:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (15.01%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,21 @@
+
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int>ans(26,0);
+        for(char ch:word){
+            ans[ch-'a']++;
+        }
+        sort(ans.begin(),ans.end(),greater<int>());
+        int a=0;
+        for(int i=0;i<26;i++){
+            if(ans[i]==0) break;
+            int cost=i/8+1;
+            a+=ans[i]*cost;
+        }
+
+
+        return a;
+        
+    }
+};
